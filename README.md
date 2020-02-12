2cc71f57cb56baf736651b23d7c457fc52b42e40
santilland <daniel.santillan@eox.at>
2020-02-12 12:11:54 +0100

    Initial commit, launcher extensions experiments

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # exampleLoader
-Testing jupyterlab customization
+
+![Github Actions Status](https://github.com/my_name/myextension/workflows/Build/badge.svg)
+
+Example loader widget to allow visualization and selection of available example notebooks
+
+
+## Requirements
+
+* JupyterLab >= 1.0
+
+## Install
+
+```bash
+jupyter labextension install exampleLoader
+```
+
+## Contributing
+
+### Install
+
+The `jlpm` command is JupyterLab's pinned version of
+[yarn](https://yarnpkg.com/) that is installed with JupyterLab. You may use
+`yarn` or `npm` in lieu of `jlpm` below.
+
+```bash
+# Clone the repo to your local environment
+# Move to exampleLoader directory
+# Install dependencies
+jlpm
+# Build Typescript source
+jlpm build
+# Link your development version of the extension with JupyterLab
+jupyter labextension link .
+# Rebuild Typescript source after making changes
+jlpm build
+# Rebuild JupyterLab after making any changes
+jupyter lab build
+```
+
+You can watch the source directory and run JupyterLab in watch mode to watch for changes in the extension's source and automatically rebuild the extension and application.
+
+```bash
+# Watch the source directory in another terminal tab
+jlpm watch
+# Run jupyterlab in watch mode in one terminal tab
+jupyter lab --watch
+```
+
+### Uninstall
+
+```bash
+jupyter labextension uninstall exampleLoader
+```
+
---
+++ src/index.ts
@@ -0,0 +1,98 @@
+import {
+  JupyterFrontEnd, JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import {ILauncher} from '@jupyterlab/launcher';
+import {IFrame} from '@jupyterlab/apputils';
+import {IDocumentManager} from '@jupyterlab/docmanager';
+
+/**
+ * Initialization data for the exampleLoader extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'exampleLoader',
+  autoStart: true,
+  requires: [ILauncher, IDocumentManager],
+  activate: loadExamples
+};
+
+export default extension;
+
+export function loadExamples(
+  app: JupyterFrontEnd, launcher: ILauncher, docmanager: IDocumentManager): Promise<void>{
+
+    // create new commands and add them to app.commands
+    function appendNewCommand(item: any) {
+        let iframe: IFrame = null;
+        let command = `vires:${item.name}`;
+        app.commands.addCommand(command, {
+            label: item.name,
+            iconClass: item.iconClass,
+            execute: () => {
+                if (item.target == '_blank') {
+                    let win = window.open(item.url, '_blank');
+                    win.focus();
+                } else if (item.target == 'widget') {
+                    if (!iframe) {
+                        iframe = new IFrame();
+                        iframe.sandbox = ['allow-scripts'];
+                        iframe.url = item.url;
+                        iframe.id = item.name;
+                        iframe.title.label = item.name;
+                        iframe.title.closable = true;
+                        iframe.node.style.overflowY = 'auto';
+                    }
+
+                    if (iframe == null || !iframe.isAttached) {
+                        app.shell.add(iframe, 'main');
+                        app.shell.activateById(iframe.id);
+                    } else {
+                        app.shell.activateById(iframe.id);
+                    }
+                }
+            }
+        });
+    }
+
+    /*appendNewCommand({
+        name: 'Example Loader',
+        target: 'widget',
+        url: 'https://edc-dev-nbviewer.hub.eox.at/localfile',
+        iconClass: 'exampleLoaderIcon'
+    });*/
+
+    appendNewCommand({
+        name: 'Example Loader',
+        target: 'widget',
+        url: 'https://swarm-vre.readthedocs.io/en/staging/',
+        iconClass: 'exampleLoaderIcon'
+    });
+
+
+    // New command for base template creation
+    app.commands.addCommand('vires:loadBaseTemplate', {
+        label: 'Load basic VirES python template',
+        iconClass: 'templateIcon',
+        execute: () => {
+            docmanager.copy('VirES/data/templateTest.ipynb', '')
+                .then((result)=>{
+                    docmanager.open(result.path);
+                });
+        }
+    });
+
+    console.log(docmanager);
+
+    launcher.add({
+        category: 'VirES',
+        command: 'vires:Example Loader',
+        rank: 0
+    });
+
+    launcher.add({
+        category: 'VirES',
+        command: 'vires:loadBaseTemplate',
+        rank: 1
+    });
+
+    return Promise.resolve(void 0);
+}
