25ba68dbe2c480e355e86ac7eb4eb7b13d0d18ed
santilland <daniel.santillan@eox.at>
2020-02-13 17:21:27 +0100

    Experimenting with loading launcher items from json file

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import {
   JupyterFrontEnd, JupyterFrontEndPlugin, IRouter
 } from '@jupyterlab/application';
 import {ILauncher} from '@jupyterlab/launcher';
-import {IFrame} from '@jupyterlab/apputils';
+//import {IFrame} from '@jupyterlab/apputils';
 import {IDocumentManager} from '@jupyterlab/docmanager';
+//import {ServerConnection} from '@jupyterlab/services';
 
 /**
  * Initialization data for the exampleLoader extension.
@@ -21,6 +22,82 @@ export function loadExamples(
   app: JupyterFrontEnd, launcher: ILauncher,
   docmanager: IDocumentManager, router: IRouter): Promise<void>{
 
+    function loadExampleNotebooks(result: string){
+        const respjson = JSON.parse(result);
+        const notebooks = respjson.notebooks;
+        for(let ii=0; ii<notebooks.length; ii++){
+            appendLauncherCommand(notebooks[ii]);
+        }
+    }
+
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+            loadExampleNotebooks(xmlHttp.responseText);
+        }
+    }
+    xmlHttp.open(
+        "GET",
+        'https://raw.githubusercontent.com/Swarm-DISC/Swarm_notebooks/master/notebooks.json',
+        true
+    );
+    xmlHttp.send(null);
+
+    /*
+    let settings = ServerConnection.makeSettings();
+    console.log(settings);
+    console.log(settings.baseUrl+'blabla');
+    ServerConnection.makeRequest('VirES/notebooks.json', {}, settings)
+        .then(result=>console.log(result));
+    */
+
+    console.log(launcher);
+    // TODO: i would like to change the name of the laucnher tile but for now
+    // i just move it to another group
+    let litems = (<any>launcher)._items;
+    console.log('launcher items');
+    console.log(litems);
+    for(let jj=0; jj<litems.length; jj++){
+        if(litems[jj].category === 'Console'){
+            litems[jj].category = 'Other';
+        }
+    }
+
+
+
+    // create new commands and add them to app.commands
+    function appendLauncherCommand(item: any) {
+        // New command for base template creation
+        let command = `vires:${item.path}`;
+        let classDesc = 'default';
+        if(item.hasOwnProperty('class')){
+            classDesc = item.class
+        }
+        let path = `shared/Swarm_notebooks/${item.path}`;
+
+        app.commands.addCommand(command, {
+            label: item.name,
+            iconClass: classDesc,
+            execute: () => {
+                docmanager.copy(path, '')
+                    .then((result)=>{
+                        docmanager.open(result.path);
+                    });
+            }
+        });
+
+        let rank = 0;
+        if(item.hasOwnProperty('rank')){
+            rank = item.rank;
+        }
+        launcher.add({
+            category: item.group,
+            command: command,
+            rank: rank
+        });
+    }
+
+    /*
     // create new commands and add them to app.commands
     function appendNewCommand(item: any) {
         let iframe: IFrame = null;
@@ -54,18 +131,6 @@ export function loadExamples(
         });
     }
 
-    router.register({
-        command: 'vires:copyRouter',
-        pattern:  /(\?copy|\&copy)([^?]+)/
-    });
-
-    /*appendNewCommand({
-        name: 'Example Loader',
-        target: 'widget',
-        url: 'https://edc-dev-nbviewer.hub.eox.at/localfile',
-        iconClass: 'exampleLoaderIcon'
-    });*/
-
     appendNewCommand({
         name: 'Example Loader',
         target: 'widget',
@@ -73,17 +138,16 @@ export function loadExamples(
         iconClass: 'exampleLoaderIcon'
     });
 
+    launcher.add({
+        category: 'VirES',
+        command: 'vires:Example Loader',
+        rank: 0
+    });
+    */
 
-    // New command for base template creation
-    app.commands.addCommand('vires:loadBaseTemplate', {
-        label: 'Load basic VirES python template',
-        iconClass: 'templateIcon',
-        execute: () => {
-            docmanager.copy('VirES/data/templateTest.ipynb', '')
-                .then((result)=>{
-                    docmanager.open(result.path);
-                });
-        }
+    router.register({
+        command: 'vires:copyRouter',
+        pattern:  /(\?copy|\&copy)([^?]+)/
     });
 
     app.commands.addCommand('vires:copyRouter', {
@@ -98,19 +162,7 @@ export function loadExamples(
         }
     });
 
-    console.log(docmanager);
 
-    launcher.add({
-        category: 'VirES',
-        command: 'vires:Example Loader',
-        rank: 0
-    });
-
-    launcher.add({
-        category: 'VirES',
-        command: 'vires:loadBaseTemplate',
-        rank: 1
-    });
 
     return Promise.resolve(void 0);
 }
